1223c9749948731970a51355e3b5906bf3555ad6
pujie <puji@gmail.com>
2018-07-04 10:57:40 +0700

    git add productadd and main to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 import { VendorsComponent } from '../app/vendors/vendors.component'
 import { ProductsComponent } from '../app/products/products.component'
+import { ProductAddComponent } from '../app/product-add/product-add.component'
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {path:'vendors',component:VendorsComponent},
-  {path:'products',component:ProductsComponent}
+  {path:'products',component:ProductsComponent},
+  {path:'productadd',component:ProductAddComponent},
+  {path:'main',component:MainComponent}
 ]
 @NgModule({
   imports: [
