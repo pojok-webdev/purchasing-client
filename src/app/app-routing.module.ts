8aab63614f39cce18220f56b17166b2c3f478304
pujie <puji@gmail.com>
2018-07-05 08:50:04 +0700

    add path to productupdate and submissions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { RouterModule, Routes } from '@angular/router'
 import { VendorsComponent } from '../app/vendors/vendors.component'
 import { ProductsComponent } from '../app/products/products.component'
 import { ProductAddComponent } from '../app/product-add/product-add.component'
+import { ProductUpdateComponent } from '../app/product-update/product-update.component'
 import { MainComponent } from './main/main.component';
+import { SubmissionAddComponent } from './submission-add/submission-add.component';
+import { SubmissionsComponent } from './submissions/submissions.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/main',pathMatch:'full'},
   {path:'vendors',component:VendorsComponent},
   {path:'products',component:ProductsComponent},
   {path:'productadd',component:ProductAddComponent},
+  {path:'productupdate/:product_id',component:ProductUpdateComponent},
+  {path:'submissionadd',component:SubmissionAddComponent},
+  {path:'submissions',component:SubmissionsComponent},
   {path:'main',component:MainComponent}
 ]
 @NgModule({
